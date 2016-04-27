b68ad4ae74f2ceb91cd40cf8d211b2a10f2af079
ipopa <ipopa@student.42.fr>
2016-04-27 20:04:12 +0200

    parse map start end

--- srcs/parse_map.c
+++ srcs/parse_map.c
@@ -4,10 +4,29 @@ int parse_map(t_map *graph)
 {
   char *line;
   int ret;
+  bool start;
+  bool end;
+
+  start = false;
+  end = false;
 
   while ((ret = get_next_line(0, &line)) >= 0)
     {
       printf("%s\n", line);
+      if (ft_strequ(line, "##start"))
+	{
+	  printf("start\n");
+	  start = true;
+	  continue;
+	}
+      else if (ft_strequ(line, "##end"))
+	{
+	  printf("end\n");
+	  end = true;
+	  continue;
+	}
+      else {
+      }
       if (ret == 0)
         break;
     }
