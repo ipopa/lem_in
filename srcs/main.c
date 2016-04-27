784c3d6981db0dff6618ffdf17bb34dafdd271aa
MoreProject <sander.beaufils@gmail.com>
2016-04-27 20:15:00 +0200

    changement du main

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,35 @@
 #include "lem_in.h"
  
+
+int init_vert(t_vert *vert)
+{
+  vert->start = false;
+  vert->end = false;
+  vert->occ = false;
+  vert->wt = 100000;
+  if ((vert->edges = (t_vert *)malloc(sizeof(t_vert))) == null)
+    return 0;
+  vert->edges = null;
+  if ((vert->orig = (t_vert *)malloc(sizeof(t_vert))) == null)
+    return 0;
+  vert->orig = null;
+  if ((vert->next = (t_vert *)malloc(sizeof(t_vert))) == null)
+    return 0;
+  vert->next = null;
+  vert->x = 0;
+  vert->y = 0;
+  return 1;
+}
+
+int init_map(t_map *graph)
+{
+  if ((graph->vertices = (t_map *)malloc(sizeof(t_map))) == null)
+    return 0;
+  graph->vertices = null;
+  graph->ants = 0;
+  return 1;
+}
+
 int main(int ac, char **av)
 {
   char *line;
@@ -12,6 +42,7 @@ int main(int ac, char **av)
 
   fd = 0;
   graph = (t_map *)malloc(sizeof(t_map));
+  init_map(graph);
   if ((ret = get_next_line(fd, &line)) == -1)
     return 0;
   if ((graph->ants = ft_atoi(line)) == 0 && !ft_strequ(line, "0"))
