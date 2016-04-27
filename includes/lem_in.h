576c1b3ce2b12ebf504e7b4e4c5b0a62158ded3f
MoreProject <sander.beaufils@gmail.com>
2016-04-27 19:00:58 +0200

    trois

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -5,12 +5,29 @@
 #include "get_next_line.h"
 
 typedef struct s_map t_map;
+typedef struct s_vert t_vert;
 
 struct s_map
 {
-  t_list *vertices;
-  t_list *edges;
+  t_vert *vertices;
   int ants;
 };
 
+<<<<<<< HEAD
+struct s_vert
+{
+  struct s_vert *edges;
+  int wt;
+  struct s_vert *orig;
+  int x;
+  int y;
+  bool start;
+  bool end;
+  bool occ;
+}
+
+int	get_next_line(int const fd, char **line);
+
+=======
+>>>>>>> d01cfc53d42f1f75e0ea2c66e0901d026e3ef0e1
 #endif
