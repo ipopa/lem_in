14c483f5f1391f2f5e86d780c96dd18f6f0269a7
ipopa <ipopa@student.42.fr>
2016-04-27 19:08:46 +0200

    ok

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -5,29 +5,12 @@
 #include "get_next_line.h"
 
 typedef struct s_map t_map;
-typedef struct s_vert t_vert;
 
 struct s_map
 {
-  t_vert *vertices;
+  t_list *vertices;
+  t_list *edges;
   int ants;
 };
 
-<<<<<<< HEAD
-struct s_vert
-{
-  struct s_vert *edges;
-  int wt;
-  struct s_vert *orig;
-  int x;
-  int y;
-  bool start;
-  bool end;
-  bool occ;
-}
-
-int	get_next_line(int const fd, char **line);
-
-=======
->>>>>>> d01cfc53d42f1f75e0ea2c66e0901d026e3ef0e1
 #endif
