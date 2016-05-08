569ae4b1f14e00a879026bf53f1feb28541984b0
ipopa <ipopa@student.42.fr>
2016-05-08 20:04:22 +0200

    push

--- srcs/print.c
+++ srcs/print.c
@@ -15,13 +15,47 @@ void print_path(t_path *path)
 {
   t_path *tmp;
 
-  tmp = path;
-  while (path != NULL)
+  tmp = path->next;
+  while (tmp != NULL)
     {
-      // print_vertice(path->vertices);
-      printf("L1-%s\n", path->vertices->name);
-      path = path->next;
+      ft_putstr(tmp->vertices->name);
+      tmp = tmp->next;
     }
+}
+
+void print_mypath(char **ants, t_path *path)
+{
+  int i;
+  t_path *tmp;
+  int tmpBool;
+
+  tmpBool = 1;
+  i = 0;
+  tmp = path->next;
+  while(tmpBool != 0)
+    {
+      while (tmp != NULL)
+	{
+	  if (!ants[i] || !ft_strcmp(tmp->vertices->name, ants[i]))
+	    {
+	      ft_putstr("L");
+	      ft_putnbr(i + 1);
+	      ft_putstr("-");
+	      ft_putstr(tmp->vertices->name);
+	      ft_putstr(" ");
+	      if (!ants[i])
+		tmpBool = 0;
+	      ants[i] = ft_strdup(tmp->vertices->name);
+	      //	      if (tmp->next == NULL)
+	      //tmpBool = 0;
+	      break;
+	    }
+	  tmp = tmp->next;
+	}
+      tmp = path->next;
+      i++;
+    }
+
 }
 
 void print_graph(t_map *graph, int **tab)
@@ -29,20 +63,28 @@ void print_graph(t_map *graph, int **tab)
   int i;
   int j;
   t_listpath *tmpLP;
+  char **ants;
 
-  i = 0;
-  while (i < tab[0][1])
+  printf("total: %d\n", tab[0][1]);
+  i = 1;
+  j = 0;
+  tmpLP = graph->listpath;
+  ants = (char **)malloc(sizeof(*ants) * (graph->ants + 1));
+  while(42)
     {
-      j = 0;
-      tmpLP = graph->listpath;
-      while (tmpLP && j < graph->nbpath)
+      print_mypath(ants, tmpLP->path);
+      
+      // print_path(tmpLP->path);
+      ft_putstr(" \n");
+      tmpLP = tmpLP->next;
+      j++;
+      if (!tmpLP)
 	{
-	  printf("nb ants %d =  %d\n", tmpLP->nbelem, tab[j][1]);
-	  print_path(tmpLP->path);
-	  printf("\n");
-	  j++;
-	  tmpLP = tmpLP->next;
+	  if (i == 2)
+	    break ;
+	  ft_putstr("\n");
+	  i++;
+	  tmpLP = graph->listpath;
 	}
-      i++;
     }
 }
