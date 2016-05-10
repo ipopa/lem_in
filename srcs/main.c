158ae24fd56f352b927c22248cc00b23d91c3569
ipopa <ipopa@student.42.fr>
2016-05-10 18:38:45 +0200

    push

--- srcs/main.c
+++ srcs/main.c
@@ -41,9 +41,9 @@ int main(int ac, char **av)
   ants = 0;
   if ((ret = get_next_line(fd, &line)) == -1)
     return 0;
-  free(line);
   if ((ants = ft_atoi(line)) == 0 && !ft_strequ(line, "0"))
     return 0;
+  free(line);
   graph = (t_map *)malloc(sizeof(t_map));
   init_map(graph);
   graph->ants = ants;
