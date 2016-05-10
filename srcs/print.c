0321e3cbba58cdf15575f682b371535b6996b1c1
ipopa <ipopa@student.42.fr>
2016-05-10 10:18:44 +0200

    push

--- srcs/print.c
+++ srcs/print.c
@@ -11,58 +11,6 @@ void print_vertice(t_vert *vert)
     printf("- vertice occuped -\n");
 }
 
-void print_path(t_path *path)
-{
-  t_path *tmp;
-
-  tmp = path->next;
-  while (tmp != NULL)
-    {
-      ft_putstr(tmp->vertices->name);
-      tmp = tmp->next;
-    }
-}
-
-void print_mypath(int *i, char **ants, t_path *path)
-{
-  int j;
-  t_path *tmp;
-  int tmpBool;
-
-  tmpBool = 1;
-  j = *i;
-  tmp = path->next;
-  while(tmpBool != 0)
-    {
-      if (ants[j])
-	{
-	  while (tmp != NULL)
-	    {
-	      if (ft_strequ(tmp->vertices->name, ants[j]))
-		{
-		  tmp = tmp->next;
-		  break ;
-		}
-	      tmp = tmp->next;
-	    }
-	}
-      else
-	{
-	  ants[j] = ft_strdup(tmp->vertices->name);
-	  tmpBool = 0;
-	}
-      if (!tmp)
-	break ;
-      ft_putstr("L");
-      ft_putnbr(j + 1);
-      ft_putstr("-");
-      ft_putstr(tmp->vertices->name);
-      ft_putstr(" ");
-      tmp = path->next;
-      j++;
-    }
-}
-
 void print_print(int i, char *str)
 {
   ft_putstr("L");
@@ -107,6 +55,47 @@ int print_ant(char **ants, t_path *path, int i)
   return 0;
 }
 
+void add_to_tab(char **tab, int nb, t_path *path, t_map *graph)
+{
+  int i;
+  char *tmpStr;
+  t_path *tmp;
+  char ant[4];
+  char *str;
+
+  tmp = path->next;
+
+  //il faut ft_itoa 
+  
+  ant[0] = 'L';
+  ant[1] = nb + 48 + 1;
+  ant[2] = '-';
+  ant[3] = 0;
+
+  i = 0;
+  while (tmp)
+    {
+      str = ft_strjoin(ant, tmp->vertices->name);
+      tmpStr = ft_strjoin(str, " ");
+      free(str);
+      if (tab[i])
+	{
+	  str = ft_strjoin(tab[i], tmpStr);
+	  free(tmpStr);
+	  free(tab[i]);
+	  tab[i] = ft_strdup(str);
+	  free(str);
+	}
+      else
+	{
+	  tab[i] = ft_strdup(tmpStr);
+	  free(tmpStr);
+	}
+      i++;
+      tmp = tmp->next;
+    }
+}
+
 void print_graph(t_map *graph, int **tab)
 {
   int i;
@@ -140,7 +129,41 @@ void print_graph(t_map *graph, int **tab)
   nb = 0;
   char *fin = ft_strdup("9");
 
-  while(42)
+  char **tabAll = (char **)malloc(sizeof(char *) * (graph->ants + 1));
+  int wpath = 1;
+  int j;
+
+  while(nb < graph->ants)
+    {
+      if (tab[wpath - 1][1] > 0)
+	{
+	  tmpLP = graph->listpath;
+	  j = 0;
+	  while (j < wpath - 1)
+	    {
+	      tmpLP = tmpLP->next;
+	      j++;
+	    }
+	  if (tmpLP)
+	    {
+	      add_to_tab(tabAll, nb, tmpLP->path, graph);
+	      tab[wpath - 1][1]--;
+	    }
+	}
+      wpath++;
+      if (wpath > graph->nbpath)
+	wpath = 1;
+      nb++;
+    }
+  j = 0;
+  while(j < graph->ants && tabAll[j])
+    {
+      printf("%s\n", tabAll[j]);
+      j++;
+    }
+
+  nb = 0;
+  while(42 == 2)
     {
       if (!nbPath[nb])
 	{
